08b410b96cc56fd5d86188aa588418ab10b9ddc5
Aravindraj6 <rajaravind963@gmail.com>
2022-08-27 11:33:32 +0530

    sample

---
+++ src/org/sel/Sample.java
@@ -0,0 +1,13 @@
+package org.sel;
+
+public class Sample {
+	
+	private void num() {
+       System.out.println("My num is 0987654321");
+	}
+	public static void main(String[] args) {
+		Sample s = new Sample();
+		s.num();
+	}
+
+}
