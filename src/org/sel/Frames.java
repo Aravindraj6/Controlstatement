06bae2bbde083b200f2830b84f7993a88e4d830b
Aravindraj6 <rajaravind963@gmail.com>
2022-08-28 10:15:56 +0530

    new change from srathi

--- src/org/sel/Frames.java
+++ src/org/sel/Frames.java
@@ -1,55 +1,47 @@
 package org.sel;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.time.Duration;
 import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import javax.net.ssl.HttpsURLConnection;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
+
 
 public class Frames {
 	
-	  public static void main(String[] args) throws InterruptedException, IOException {
-		
-		  System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\eclipse-workspace\\Selenium\\Driver\\chromedriver.exe");
-		  
-		  WebDriver driver = new ChromeDriver();
-		  
-		  driver.get("https://www.facebook.com/");
-		  
-          List<WebElement> allLinks = driver.findElements(By.tagName("a"));
-          
-          int s = allLinks.size();
-          System.out.println(s);
-          
-          for (WebElement link : allLinks) {
-			String  at= link.getAttribute("href");
-			System.out.println(at);
+	
+	  public static void main(String[] args) {
 			
-			if (at != null) {
-				URL u = new URL(at);
-				URLConnection op = u.openConnection();
-				HttpsURLConnection hp = (HttpsURLConnection)op;
+      	System.setProperty("webdriver.chrome.driver",
+      			"C:\\Users\\DELL\\eclipse-workspace\\Selenium\\Driver\\chromedriver.exe");
+      	
+      	WebDriver driver = new ChromeDriver();
+      	
+      	driver.get("http://demo.guru99.com/test/write-xpath-table.html");
+      	
+      	List<WebElement> tRows = driver.findElements(By.tagName("tr"));
+      	
+      	for (int i = 0; i < tRows.size(); i++) {
+      		
+      		WebElement trow = tRows.get(i);
+      		
+      	}
+      	List<WebElement> tdatas = driver.findElements(By.tagName("td"));
+      			        		
+      			   for (int j = 0; j < tdatas.size(); j++) {
+      				WebElement tdata = tdatas.get(j);
+      				String text = tdata.getText();
+      				System.out.println(text);
+
 				
-				int code = hp.getResponseCode();
-				if (code >=400) {
-					System.out.println(at);
-				}
 			}
-		}
-		
-		
-	}
+			}
+      	
+      	
+	
+	
 
 }
